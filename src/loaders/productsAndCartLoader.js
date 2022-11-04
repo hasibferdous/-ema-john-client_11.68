ad8c17eb7c8a4e0f7f11a7db50d16505509406ac
hasibferdous <hasibferdous.ict@gmail.com>
2022-11-05 01:29:44 +0600

    update

--- src/loaders/productsAndCartLoader.js
+++ src/loaders/productsAndCartLoader.js
@@ -3,14 +3,14 @@ import { getStoredCart } from "../utilities/fakedb";
 export const productsAndCartLoader = async () => {
     // get Products
     const productsData = await fetch('http://localhost:5001/products');
-    const products = await productsData.json();
+    const { products } = await productsData.json();
 
     // get cart
     const savedCart = getStoredCart();
     const initialCart = [];
     
     for (const id in savedCart) {
-        const addedProduct = products.find(product => product.id === id);
+        const addedProduct = products.find(product => product._id === id);
         if (addedProduct) {
             const quantity = savedCart[id];
             addedProduct.quantity = quantity;
